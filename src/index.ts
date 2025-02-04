adca693858306beb93711f06083e52eaa3828b5e
Michael <pinke@live.ru>
2025-02-04 11:18:09 +0300

    v2.

--- src/index.ts
+++ src/index.ts
@@ -2,48 +2,61 @@ import { compose, split, uniq, length, equals, map, fromPairs } from "pepka"
 
 const { floor } = Math
 
-let abc: string,
-    abclen: number,
-    c2pos: {[char: string]: number},
-    standard: boolean,
-    hex = '0123456789abcdefghijklmnopqrstuvwxyz'
+const hex = '0123456789abcdefghijklmnopqrstuvwxyz'
 
 const isUniq = (s: string) => compose(equals(length(s)), length, uniq, split(''))(s)
 const getc2pos = compose(fromPairs, map((v, i) => [v, i]), split(''))
 const isStandard = (abc: string) => hex.startsWith(abc)
 
-/** Sets an alphabet to encode into.
- * @param {string} [newABC] - all symbols must be unique.
-*/
-export const setABC = (newABC: string) => {
-  if(!isUniq(newABC)) throw new Error('Not all chars are unique!')
-  abc = newABC
-  abclen = abc.length
-  standard = isStandard(abc)
-  c2pos = getc2pos(abc)
-}
-setABC(hex+'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-
-/** Zips number to a string by encoding into given alphabet.
- * @param {number} [n] - Number to zip.
-*/
-export const zipnum = (n: number): string => {
-  let s: string = ''
-  while(n > 0) {
-    s = abc[n%abclen]+s
-    n = floor(n/abclen)
+export class Zipnum {
+  private abc: string
+  private abclen: number
+  private c2pos: {[char: string]: number}
+  private standard: boolean
+  /** Sets an alphabet to encode into.
+   * @param {string} [newABC] - all symbols must be unique.
+  */
+  public setABC(newABC: string) {
+    if(!isUniq(newABC)) throw new Error('Not all chars are unique!')
+    this.abc = newABC
+    this.abclen = newABC.length
+    this.standard = isStandard(newABC)
+    this.c2pos = getc2pos(newABC)
   }
-  return s || '0'
+  /** Zips number to a string by encoding into given alphabet.
+   * @param {number} [n] - Number to zip.
+  */
+  public zip(n: number): string {
+    const {abc, abclen} = this
+    let s: string = ''
+    while(n > 0) {
+      s = abc[n%abclen]+s
+      n = floor(n/abclen)
+    }
+    return s || '0'
+  }
+  /** Unzips previously packed string back to a number.
+   * @param {string} [s] - String to unzip.
+  */
+  public unzip(s: string): number {
+    const {standard, abclen, c2pos} = this
+    if(standard) return parseInt(s, abclen)
+    const len = s.length
+    let n = 0
+    for(let i=0; i<len; i++)
+      n += c2pos[s[i]] * abclen**(len-i-1)
+    return n
+  }
+  constructor(abc?: string) {
+    this.setABC(abc || hex+'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+  }
 }
 
-/** Unzips previously packed string back to a number.
- * @param {string} [s] - String to unzip.
-*/
-export const unzipnum = (s: string): number => {
-  if(standard) return parseInt(s, abclen)
-  const len = s.length
-  let n = 0
-  for(let i=0; i<len; i++)
-    n += c2pos[s[i]] * abclen**(len-i-1)
-  return n
-}
+// Next lines are for v1 compatibility.
+const instance = new Zipnum()
+/** @deprecated Use basic const zipnum = new Zipnum(); zipnum.setABC() instead. */
+export const setABC = instance.setABC.bind(instance) as Zipnum['setABC']
+/** @deprecated Use basic const zipnum = new Zipnum(); zipnum.zip() instead. */
+export const zipnum = instance.zip.bind(instance) as Zipnum['zip']
+/** @deprecated Use basic const zipnum = new Zipnum(); zipnum.unzip() instead. */
+export const unzipnum = instance.unzip.bind(instance) as Zipnum['setABC']
